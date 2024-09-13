e2e7182c2246d729ad7d1f338ef22ed95264761e
sumitra tamang <sumi8tamang@gmail.com>
2024-09-13 10:08:02 +0545

    feat: creates footer component

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+<template>
+<div>
+    <ul class="flex">
+        <li><i class="fa-brands fa-facebook"></i></li>
+        <li><i class="fa-brands fa-instagram"></i></li>
+        <li><i class="fa-brands fa-twitter"></i></li>
+        <li><i class="fa-brands fa-youtube"></i></li>
+    </ul>
+</div>
+</template>
