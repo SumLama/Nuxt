c70c7b116860911ce82b89e4cd70298b40e64f82
sumitra tamang <sumi8tamang@gmail.com>
2024-09-13 11:49:45 +0545

    chore: project re-setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  components: true, 
   plugins: [
     '~/plugins/fontawesome.js'
   ],
-  css: ['~/assets/css/main.css']
+  css: [
+    '~/assets/css/main.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ]
+
 })
