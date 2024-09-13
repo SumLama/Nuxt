5a025bb54106bdc1bd6232e0e50b510c40f73c1e
sumitra tamang <sumi8tamang@gmail.com>
2024-09-13 10:09:11 +0545

    feat: creates header component

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+<template>
+    <ul class="flex justify-center gap-12 text-lg text-teal-50 cursor-pointer py-2  ">
+        <li><NuxtLink to='/'>Home</NuxtLink></li>
+        <li><NuxtLink to='/about'>About</NuxtLink></li>
+        <li><NuxtLink to='/contactUs'>Contact us</NuxtLink></li>
+        <li>
+            <ul class="flex gap-3">
+                <li><FontAwesomeIcon icon="fa-brands fa-linkedin" /></li>
+                <li><FontAwesomeIcon icon="fa-brands fa-facebook" /></li>
+                <li><FontAwesomeIcon icon="fa-brands fa-instagram" /></li>
+                <li><FontAwesomeIcon icon="fa-brands fa-twitter" /></li>
+             </ul>
+        </li>
+    </ul>
+</template>    
